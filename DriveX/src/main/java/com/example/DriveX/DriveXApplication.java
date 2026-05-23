3757c2c3307ebad896dec7aea5987d09af858ea4
shuvo37 <mdhasanmehedi5237@gmail.com>
2026-05-23 13:25:42 +0600

    first

---
+++ DriveX/src/main/java/com/example/DriveX/DriveXApplication.java
@@ -0,0 +1,13 @@
+package com.example.DriveX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriveXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriveXApplication.class, args);
+	}
+
+}
---
+++ DriveX/src/test/java/com/example/DriveX/DriveXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DriveX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriveXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
