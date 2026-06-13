6298bed8ed77eaefbb01252169619a3c50c8b480
shuvo37 <mdhasanmehedi5237@gmail.com>
2026-06-13 13:02:52 +0600

    implented JwrUtil.java.

---
+++ DriveX/src/main/java/com/example/DriveX/config/JWT/JwtUtil.java
@@ -0,0 +1,88 @@
+package com.example.DriveX.config.JWT;
+
+import java.security.Key;
+import java.util.Date;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import io.jsonwebtoken.security.Keys;
+
+@Component
+public class JwtUtil {
+
+  @Value("${jwt.secret}")
+  private String secret;
+
+  @Value("${jwt.expiration-ms}")
+  private String expirationMs;
+
+
+  private Key getSigningKey()
+  {
+
+         return Keys.hmacShaKeyFor(secret.getBytes());
+
+  }
+
+
+  public String generateToken(String email)
+  {
+
+      return Jwts.builder()
+              .setSubject(email)
+              .setIssuedAt(new Date())
+              .setExpiration(new Date(System.currentTimeMillis() + expirationMs))
+              .signWith(getSigningKey() , SignatureAlgorithm.HS256)
+              .compact();
+
+
+  }
+
+  public String extractEmail(String token)
+  {
+
+       return ParseClaims(token).getSubject();
+
+  }
+
+
+  public boolean isTokenValid(String token)
+  {
+
+         try{
+
+             ParseClaims(token);
+
+             return  true;
+
+         }
+         catch(Exception e)
+         {
+
+             System.out.println(e.getMessage());
+
+             return false;
+
+         }
+
+  }
+
+
+  private Claims ParseClaims(String token)
+  {
+
+      return Jwts.parserBuilder()
+              .setSigningKey(getSigningKey())
+              .build()
+              .parseClaimsJws(token)
+              .getBody();
+
+  }
+
+
+
+
+}
